f5162a022f14cc2e9ff2cadab01f529bb2bc40ab
LukaPodbregar <33podbregarl@gmail.com>
2021-11-19 15:03:07 +0100

    Init commit

---
+++ app/src/main/java/si/uni_lj/fe/seminar/Login.java
@@ -0,0 +1,21 @@
+package si.uni_lj.fe.seminar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+
+public class Login extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+}
